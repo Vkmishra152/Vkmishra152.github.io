6aa88a3264abe343d4fe7aec41df5b2d059af982
Vkmishra152 <79801285+Vkmishra152@users.noreply.github.com>
2021-07-15 16:44:20 +0530

    Add files via upload

---
+++ Bhinder Tour & Travels/booking.js
@@ -0,0 +1,69 @@
+  function validateName() {
+            var name = document.getElementById('name').value;
+            if(name.length == 0) {
+              alert("Name can't be blank") ;
+              return false;
+
+            }
+            if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+              alert("Please enter your correct name") ;//Validation Message
+              return false;
+            }
+            return true;
+          }
+
+
+         function validateEmail () {
+
+          var email = document.getElementById('email').value;
+          if(email.length == 0) {
+            alert("Email can't be blank") ;//Validation Message
+            return false;
+
+          }
+
+          if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+            alert("Please enter a correct email address") ;//Validation Message
+            return false;
+
+          }
+
+          return true;
+
+        }
+
+        var currentDateTime = new Date();
+          var year = currentDateTime.getFullYear();
+          var month = (currentDateTime.getMonth() + 1);
+          var date = (currentDateTime.getDate() + 1);
+
+          if(date < 10) {
+          date = '0' + date;
+          }
+          if(month < 10) {
+          month = '0' + month;
+          }
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var checkinElem = document.querySelector("#checkin-date");
+var checkoutElem = document.querySelector("#checkout-date");
+
+checkinElem.setAttribute("min", dateTomorrow);
+
+checkinElem.onchange = function () {
+    checkoutElem.setAttribute("min", this.value);
+}
+
+
+        function validateForm() {
+          if (!validateName() || !validatePhone() || !validateEmail()) {
+
+            alert("Form not submitted");//Validation Message
+            return false;
+          }
+          else {
+            submitted=true;
+            return true;
+          }
+        }
+       
